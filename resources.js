3f0b0fc8bedb6d4341260594a29dbad380cacc94
Robert Wagner <rwwagner@vt.edu>
2014-02-23 15:21:50 -0500

    Added my Angular Boilerplate stuff to start

---
+++ resources.js
@@ -0,0 +1,11 @@
+head.js(
+    /* External libraries */
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/angular/angular.js',
+    'bower_components/angular-route/angular-route.js',
+    'bower_components/angular-animate/angular-animate.js',
+    /* App.js */
+    'scripts/app.js',
+    /* Controllers */
+    'scripts/controllers/home.js'
+);
---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', ['ngRoute', 'ngAnimate'])
+    .config(function ($routeProvider, $locationProvider, $provide) {
+        $routeProvider.when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'HomeController'
+        });
+        $routeProvider.otherwise({ redirectTo: '/' });
+    });
+
+/*app.factory('currentUser', ['$http', function ($http) {
+    return {
+        username: function (callback) {
+            $http.get('/currentuser').success(function (response) {
+                callback(response);
+            })
+                .error(function (error) {
+                    alert("Something blew up!");
+                });
+        }
+    };
+}]);*/
